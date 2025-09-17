fe253537340cbae1014245c12eca75f990d96fc5
Voros <wesgamelogin@gmail.com>
2025-09-16 19:18:59 -0500

    Readded recipes for these two fuckass mods bc they broke for no goddamn reason

--- kubejs/server_scripts/mods/FunctionalStorage.js
+++ kubejs/server_scripts/mods/FunctionalStorage.js
@@ -1,7 +1,37 @@
-ServerEvents.recipes((event) => {
-  event.replaceInput(
-    { mod: "functionalstorage" },
-    "#c:chests/wooden",
-    "modern_industrialization:bronze_gear"
-  )
+ServerEvents.recipes ((event) => {
+    event.replaceInput(
+        {mod:"functionalstorage"},
+        "#c:chests/wooden",
+        "modern_industrialization:bronze_gear"
+    );
+    event.replaceInput(
+        {mod:"functionalstorage"},
+        "minecraft:bucket",
+        "modern_industrialization:stainless_steel_tank"
+    );
+    event.remove( {output: 'functionalstorage:copper_upgrade'})
+    event.shaped('functionalstorage:copper_upgrade', ["PGP", "GSG", "PGP"], {
+        P: 'modern_industrialization:steel_plate',
+        G: 'modern_industrialization:steel_gear',
+        S: '#functionalstorage:drawer',
+    });
+    event.remove( {output: 'functionalstorage:gold_upgrade'})
+    event.shaped('functionalstorage:gold_upgrade', ["PGP", "GUG", "PGP"], {
+        P: 'modern_industrialization:aluminum_plate',
+        G: 'modern_industrialization:aluminum_gear',
+        U: 'functionalstorage:copper_upgrade',
+    });
+    event.remove( {output: 'functionalstorage:diamond_upgrade'})
+    event.shaped('functionalstorage:diamond_upgrade', ["PGP", "GUG", "PGP"], {
+        P: 'modern_industrialization:stainless_steel_plate',
+        G: 'modern_industrialization:stainless_steel_gear',
+        U: 'functionalstorage:gold_upgrade',
+    });
+    event.remove( {output: 'functionalstorage:netherite_upgrade'})
+    event.shaped('functionalstorage:netherite_upgrade', ["PGP", "GUG", "PGP"], {
+        P: 'modern_industrialization:titanium_plate',
+        G: 'modern_industrialization:titanium_gear',
+        U: 'functionalstorage:diamond_upgrade',
+    });
+
 });
