f6ff3519465eb76fc96fbf5370f462d9d5933ada
fmbellomy <fmbellomy@quantumgarbage.com>
2025-09-17 18:25:54 -0500

    remove copper stack upgrade from emi

--- kubejs/server_scripts/nukes/Redundancies.js
+++ kubejs/server_scripts/nukes/Redundancies.js
@@ -1,3 +1,9 @@
 RecipeViewerEvents.removeEntriesCompletely("item", (event) => {
   event.remove("minecraft:nether_gold_ore");
+  event.remove("sophisticatedbackpacks:stack_upgrade_starter_tier");
+});
+
+ServerEvents.recipes((event) => {
+  event.remove({ output: "sophisticatedbackpacks:stack_upgrade_starter_tier" });
+  event.remove({ input: "sophisticatedbackpacks:stack_upgrade_starter_tier" });
 });
