a46f6de072978992ad6c009333633ae8653548fc
fmbellomy <fmbellomy@quantumgarbage.com>
2025-09-25 14:16:29 -0500

    make ender pearls obtainable in peaceful

---
+++ kubejs/startup_scripts/Fluids.js
@@ -0,0 +1,10 @@
+MIRegistrationEvents.registerFluids((event) => {
+  event.register(
+    "Uraninite Slush",
+    "uraninite_slush",
+    0x1b1b1b,
+    "water",
+    false,
+    "medium"
+  );
+});
