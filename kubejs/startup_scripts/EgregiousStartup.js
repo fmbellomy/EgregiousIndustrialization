e6f8f2461a882d1d599762c211b388c4da1fb96f
fmbellomy <fmbellomy@quantumgarbage.com>
2025-09-28 13:03:52 -0500

    lens textures

--- kubejs/startup_scripts/EgregiousStartup.js
+++ kubejs/startup_scripts/EgregiousStartup.js
@@ -1,9 +1,22 @@
 Platform.mods.kubejs.name = "Egregious Industrialization";
 
 StartupEvents.registry("item", (event) => {
-  const lenses = ["diamond", "glass", "emerald", "certus", "quartz"];
+  const lenses = [
+    { mat: "diamond", color: "#4AEAD6" },
+    { mat: "glass", color: "#BABDB5" },
+    { mat: "emerald", color: "#40F082" },
+    { mat: "certus", color: "#88B9EC" },
+    { mat: "quartz", color: "#E2DCD3" },
+  ];
   lenses.forEach((lens) => {
-    event.create(`${lens}_lens`);
+    event
+      .create(`${lens.mat}_lens`)
+      .textures({
+        layer0: "kubejs:item/lens",
+        layer1: "kubejs:item/lens_secondary",
+        layer2: "kubejs:item/lens_overlay",
+      })
+      .color(0, lens.color);
   });
   const wafers = ["cpu", "ilc", "nand", "nor", "ram", "soc"];
   wafers.forEach((wafer) => {
