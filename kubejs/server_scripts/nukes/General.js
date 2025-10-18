61ed4a26d03f22bfc06e97d2d0e8e76a59c01d10
fmbellomy <fmbellomy@quantumgarbage.com>
2025-10-18 11:34:03 -0500

    nuke multi processing array because it is boring

---
+++ kubejs/server_scripts/nukes/General.js
@@ -0,0 +1,11 @@
+let nukelist = [];
+nukelist.push("industrialization_overdrive:multi_processing_array");
+
+nukelist.forEach((nuke) => {
+  ServerEvents.recipes((event) => {
+    event.remove(nuke);
+  });
+  RecipeViewerEvents.removeEntriesCompletely("item", (event) => {
+    event.remove(nuke);
+  });
+});
